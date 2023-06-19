39eb71bc0d710558975e930689298b417df967c5
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-06-19 13:13:58 +0530

    insert method added

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -20,15 +20,18 @@ Node<DATA_TYPE>* Search(const DATA_TYPE&);
 		//TODO Sorting
 void sort();
 		//TODO Insert
-void insert(const DATA_TYPE&);
+void insert(const unsigned int&,const DATA_TYPE&);
 		//TODO delete
 void remove(const DATA_TYPE&);
 void append(const DATA_TYPE&);//O(1)
 void display()const;//O(n)
+auto get_length()const { return this->len; };
+
 private:
 
 Node<DATA_TYPE>* head_node = nullptr;
 Node<DATA_TYPE>* last_node = nullptr;//last or tail node 
+unsigned int len{};
 };
 }
 MACRO_LINKEDLIST
@@ -41,8 +44,32 @@ void V::LinkedList<DATA_TYPE>::sort()
 {
 }
 MACRO_LINKEDLIST
-void V::LinkedList<DATA_TYPE>::insert(const DATA_TYPE&)
+void V::LinkedList<DATA_TYPE>::insert(const unsigned int&index, const DATA_TYPE&data)
 {
+	if (index > len)return;
+	++len;
+	V::Node<DATA_TYPE>* node = new Node<DATA_TYPE>();
+	node->data = data;
+	if (index == 0)
+	{
+		node->next = head_node;
+		head_node = node;
+	}
+	else if (index==len)
+	{
+		last_node = last_node->next = node;
+	}
+	else
+	{
+		auto temp_head = head_node
+			;
+		for (int i = 0; i < index && temp_head != nullptr; ++i)
+		{
+			temp_head = temp_head->next;
+		};
+		node->next = temp_head->next;
+		temp_head->next = node;
+	}
 }
 MACRO_LINKEDLIST
 void V::LinkedList<DATA_TYPE>::remove(const DATA_TYPE&)
@@ -51,7 +78,7 @@ void V::LinkedList<DATA_TYPE>::remove(const DATA_TYPE&)
 MACRO_LINKEDLIST
 void V::LinkedList<DATA_TYPE>::append(const DATA_TYPE& data)//O(1)
 {
-
+	++len;
 	Node<DATA_TYPE>* node = new Node<DATA_TYPE>();
 	node->data = data;
 	node->next = nullptr;
--- main.cpp
+++ main.cpp
@@ -1,18 +1,45 @@
 // main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include"linkedlist.hpp"
+#include<fstream>
+#include<string>
 int main()
 {
     V::LinkedList<std::string>string_list;
     V::LinkedList<int>int_list;
+    //std::fstream file;
+    //while (true)
+    //{
 
+    //    printf("file path <> : ");
+    //std::string filename{};
+    //std::getline(std::cin, filename);
 
-    for (int i = 0; i < 150; ++i)int_list.append(i * i);
-    string_list.append("Hello");//O(1)
-    string_list.append("World");
-    string_list.append("!");
-    string_list.append("by Vishal!\n");
+    //file.open(filename.c_str(), std::fstream::in);
+    //if (file.is_open())
+    //{
+    //    std::string str{};
+    //    while (!file.eof())
+    //    {
+
+    //    char c=file.get();
+    //    str += c;
+    //    }
+    //    string_list.append(str);
+    //    file.close();
+    //}
+    //else
+    //    printf("file doesn't exist!\n");
+    string_list.append("First");
+    string_list.append(" Second");
+    string_list.insert(3, "Third");
+    string_list.insert(0, "Fourth");
+    string_list.insert(1, "Random");
+    string_list.insert(2, "2Random");
+    string_list.insert(6, "2435Random");
+    for (int i = 0; i < 5; ++i)int_list.append(i * i);
     string_list.display();//O(n)
     int_list.display();
+    
+    getchar();
 }
-
